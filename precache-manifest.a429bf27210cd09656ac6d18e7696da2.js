eb46d2471da9e9be71e6cb59be352fb9903b124b
unknown <engineer.aslamhossain@gmail.com>
2020-09-06 11:16:28 +0600

    Updates

---
+++ precache-manifest.a429bf27210cd09656ac6d18e7696da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c37fec5fc7d5a12db0e0c60ec12003c",
+    "url": "/social-buddy/index.html"
+  },
+  {
+    "revision": "199ab284b642f277674a",
+    "url": "/social-buddy/static/css/main.697f61a7.chunk.css"
+  },
+  {
+    "revision": "38af50ee4175c33012f4",
+    "url": "/social-buddy/static/js/2.1565d77d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/social-buddy/static/js/2.1565d77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199ab284b642f277674a",
+    "url": "/social-buddy/static/js/main.4217dfc3.chunk.js"
+  },
+  {
+    "revision": "08a5aa5d4c4f7df5deeb",
+    "url": "/social-buddy/static/js/runtime-main.f1f34df6.js"
+  }
+]);
